671d5ef55fee0662b07b9922ec5d413551a7cf35
insu1213 <32616560+insu1213@users.noreply.github.com>
2021-12-01 10:17:45 +0900

    12/1 프로그래밍

--- 개인프로젝트/개인프로젝트/main.c
+++ 개인프로젝트/개인프로젝트/main.c
@@ -92,123 +92,157 @@ void insu(int j) {
 	}
 }
 
-void basic() {
+void basic(int point, int ck, int inputck) {
 	gotoxy(0, 18);
-	printf("------------------------\n");
+
+
+	printf("□□□□□□□□□□□□\n");
+
 	gotoxy(0, 0);
 	printf("------------------------\n");
+	gotoxy(27, 10);
+	printf("%d 점", point);
+	gotoxy(10, 8);
+	printf("%.2f %%", (double)point / (300 * ck) * 100);
 	gotoxy(0, 1);
 }
 
 int main() {
 	int s = 0;
+	int ck = 1;
 	clock_t timer = clock();
-	basic();
 	int point = 0;
+	basic(point, ck, 0);
 
 	while (s < sizeof(note) / sizeof(note[0])) {
 
 		if (_kbhit()) {
 			int c = _getch();
 			/*printf("입력 값 : %c\n", c);*/
+
+
 			int tmp = s - 10;
 			if (tmp > 0) {
 				if (note[tmp][0] == 1) {
 					if (c == 'a') {
+						ck++;
 						gotoxy(1, 20);
-						if (80 < clock() - timer && clock() - timer < 120) {
+						if (30 < clock() - timer && clock() - timer < 100) {
 							printf("BEST");
 							point += 300;
+							
 						}
-						else if ((clock() - timer <= 80 && clock() - timer > 50) || (clock() - timer >= 120 && clock() - timer < 150)) {
+						else if ((clock() - timer <= 30 && clock() - timer > 0) || (clock() - timer >= 100 && clock() - timer < 200)) {
 							printf("GOOD");
 							point += 100;
+						
 						}
-						else if (clock() - timer <= 50) {
+						else if (clock() - timer <= 0) {
 							printf("SLOW");
 							point += 50;
+						
 						}
-						else if (clock() - timer >= 150) {
+						else if (clock() - timer >= 200) {
 							printf("FAST");
 							point += 50;
+							
 						}
 						else {
 							printf("MISS");
+							
 						}
 					}
 				}
 
 				if (note[tmp][1] == 1) {
 					if (c == 's') {
+						ck++;
 						gotoxy(7, 20);
-						if (80 < clock() - timer && clock() - timer < 120) {
+						if (30 < clock() - timer && clock() - timer < 100) {
 							printf("BEST");
 							point += 300;
+
 						}
-						else if ((clock() - timer <= 80 && clock() - timer > 50) || (clock() - timer >= 120 && clock() - timer < 150)) {
+						else if ((clock() - timer <= 30 && clock() - timer > 0) || (clock() - timer >= 100 && clock() - timer < 200)) {
 							printf("GOOD");
 							point += 100;
+
 						}
-						else if (clock() - timer <= 50) {
+						else if (clock() - timer <= 0) {
 							printf("SLOW");
 							point += 50;
+
 						}
-						else if (clock() - timer >= 150) {
+						else if (clock() - timer >= 200) {
 							printf("FAST");
 							point += 50;
+
 						}
 						else {
 							printf("MISS");
+
 						}
 					}
 				}
 
 				if (note[tmp][2] == 1) {
 					if (c == ';') {
+						ck++;
 						gotoxy(13, 20);
-						if (80 < clock() - timer && clock() - timer < 120) {
+						if (30 < clock() - timer && clock() - timer < 100) {
 							printf("BEST");
 							point += 300;
+
 						}
-						else if ((clock() - timer <= 80 && clock() - timer > 50) || (clock() - timer >= 120 && clock() - timer < 150)) {
+						else if ((clock() - timer <= 30 && clock() - timer > 0) || (clock() - timer >= 100 && clock() - timer < 200)) {
 							printf("GOOD");
 							point += 100;
+
 						}
-						else if (clock() - timer <= 50) {
+						else if (clock() - timer <= 0) {
 							printf("SLOW");
 							point += 50;
+
 						}
-						else if (clock() - timer >= 150) {
+						else if (clock() - timer >= 200) {
 							printf("FAST");
 							point += 50;
+
 						}
 						else {
 							printf("MISS");
+
 						}
 					}
 				}
 
 				if (note[tmp][3] == 1) {
 					if (c == '\'') {
+						ck++;
 						gotoxy(19, 20);
-						if (80 < clock() - timer && clock() - timer < 120) {
+						if (30 < clock() - timer && clock() - timer < 100) {
 							printf("BEST");
 							point += 300;
+
 						}
-						else if ((clock() - timer <= 80 && clock() - timer > 50) || (clock() - timer >= 120 && clock() - timer < 150)) {
+						else if ((clock() - timer <= 30 && clock() - timer > 0) || (clock() - timer >= 100 && clock() - timer < 200)) {
 							printf("GOOD");
 							point += 100;
+
 						}
-						else if (clock() - timer <= 50) {
+						else if (clock() - timer <= 0) {
 							printf("SLOW");
 							point += 50;
+
 						}
-						else if (clock() - timer >= 150) {
+						else if (clock() - timer >= 200) {
 							printf("FAST");
 							point += 50;
+
 						}
 						else {
 							printf("MISS");
+
 						}
 					}
 				}
@@ -241,7 +275,7 @@ int main() {
 
 			s++;
 			timer = clock();
-			basic();
+			basic(point, ck, 0);
 		}
 
 		
